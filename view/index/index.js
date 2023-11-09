a7f0053bd5e7a03b922ea87cbcd004c8b912cdc5
fengjutian <>
2023-11-09 16:57:34 +0800

    speech

--- view/index/index.js
+++ view/index/index.js
@@ -70,3 +70,45 @@ document.querySelector('#add-btn').addEventListener('click', () => {
 
     render()
 })
+
+
+document.querySelector('#save-voice-btn').addEventListener('click', () => {
+  try {
+    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+  } catch(e) {
+    console.error(e);
+  }
+
+  recognition.onstart = function() { 
+    instructions.text('语音识别功能激活！请对着麦克风讲话。');
+  }
+   
+  recognition.onspeechend = function() {
+    instructions.text('长时间未说话，已自动关闭录音。');
+  }
+   
+  recognition.onerror = function(event) {
+    if(event.error == 'no-speech') {
+      instructions.text('未检测到语音，请再试一次。');  
+    };
+  }
+
+  recognition.onresult = function(event) {
+ 
+    // event 是一个SpeechRecognitionEvent 对象
+    // 保存了所有历史捕获对象
+    // 我们只取当前的内容
+    var current = event.resultIndex;
+   
+    // 获取此前所说话的记录
+    var transcript = event.results[current][0].transcript;
+   
+    // 将当前记录添加到笔记内容中
+    noteContent += transcript;
+    noteTextarea.val(noteContent);
+  }
+
+
+    
+})
